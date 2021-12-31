20b78557823501ebab800e6b4ceeb2bbe4d5bae2
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-12-31 19:08:10 -0300

    Push

--- .test/index.js
+++ .test/index.js
@@ -1,9 +1,23 @@
 const chai = require("chai");
 const { assert } = chai;
-const VersionManeger = require("./");
+const VersionManeger = require("../src/cjs/index");
 
 describe("Find Version", () => {
-  it("Local Find", async () => {
-    const LocalVersion = VersionManeger
+  it("Local Find Bedrock Syncronous", function(done) {
+    const LocalVersion = VersionManeger.find("latest", "bedrock");
+    console.log(LocalVersion);
+    return done();
+  });
+  it("Local Find Bedrock Asyncronous", async function() {
+    const LocalVersion = await VersionManeger.findAsync("latest", "bedrock");
+    console.log(LocalVersion);
+    return;
+  });
+  it("Local Find Bedrock Asyncronous Callback", function(done) {
+    VersionManeger.findCallback("latest", "bedrock", (err, Data) => {
+      console.log(err, Data);
+      if (err) return done(err);
+      return done();
+    });
   });
 });
--- src/cjs/index.js
+++ src/cjs/index.js
@@ -1,4 +1,5 @@
 const Axios = require("axios");
+const util = require("util");
 const fs = require("fs");
 const path = require("path");
 
@@ -6,26 +7,22 @@ const path = require("path");
 const GithubRawUrl = "https://raw.githubusercontent.com/The-Bds-Maneger/ServerVersions/main";
 const Mod = {};
 
-Mod.findSync = function(ServerVersion = "latest", ServerPlatform = "bedrock") {
-  let VersionsList = require("../Versions.json");
-  VersionsList = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../Versions.json"), "utf8"));
+function MainFunctionFind(ServerVersion = "latest", ServerPlatform = "bedrock", VersionsList = require("../Versions.json")) {
   if (typeof ServerVersion !== "string") throw new Error("ServerVersion must be a string");
   if (typeof ServerPlatform !== "string") throw new Error("ServerPlatform must be a string");
   ServerVersion = ServerVersion.toLowerCase(); ServerPlatform = ServerPlatform.toLowerCase();
   if (ServerVersion === "latest") {
     ServerVersion = VersionsList.latest[ServerPlatform];
   }
+  //
+  // ------------------------------------------------------------------------------------------------------------
   // Script
-
+  //
   const VersionFiltred = (VersionsList.platform.filter(SV => SV.version === ServerVersion && SV.name === ServerPlatform))[0];
   if (VersionFiltred === undefined) throw new Error("Version not found");
   const DataReturn = {
     ".raw": VersionFiltred,
     url: "",
-    GetBuffer: async () => {
-      const Buffer = await Axios.get(DataReturn.url, { responseType: "arraybuffer" });
-      return Buffer.data;
-    },
     version: "",
     Date: new Date(),
   }
@@ -49,52 +46,40 @@ Mod.findSync = function(ServerVersion = "latest", ServerPlatform = "bedrock") {
   return DataReturn;
 }
 
-Mod.findAsync = async function(ServerVersion = "latest", ServerPlatform = "bedrock") {
-  const VersionsList = (await Axios.get(`${GithubRawUrl}/src/Versions.json`)).data;
-  fs.writeFileSync(path.resolve(__dirname, "../Versions.json"), JSON.stringify(VersionsList, null, 2));
-  if (typeof ServerVersion !== "string") throw new Error("ServerVersion must be a string");
-  if (typeof ServerPlatform !== "string") throw new Error("ServerPlatform must be a string");
-  ServerVersion = ServerVersion.toLowerCase(); ServerPlatform = ServerPlatform.toLowerCase();
-  if (ServerVersion === "latest") {
-    ServerVersion = VersionsList.latest[ServerPlatform];
-  }
-  // Script
+/**
+ * Look for a server version with cached files saved from when the package was installed and when the findCallback and findAsync function is used
+ */
+Mod.find = (ServerVersion = "latest", ServerPlatform = "bedrock") => MainFunctionFind(ServerVersion, ServerPlatform, JSON.parse(fs.readFileSync(path.resolve(__dirname, "../Versions.json"), "utf8")));
 
-  const VersionFiltred = (VersionsList.platform.filter(SV => SV.version === ServerVersion && SV.name === ServerPlatform))[0];
-  if (VersionFiltred === undefined) throw new Error("Version not found");
-  const DataReturn = {
-    ".raw": VersionFiltred,
-    url: "",
-    GetBuffer: async () => {
-      const Buffe = await Axios.get(DataReturn.url, { responseType: "arraybuffer" });
-      return Buffer.from(Buffe.data);
-    },
-    version: "",
-    Date: new Date(),
-  }
-  if (typeof VersionFiltred.data.url === "string") {
-    DataReturn.url = VersionFiltred.data.url;
-    DataReturn.version = VersionFiltred.version;
-    DataReturn.Date = new Date(VersionFiltred.Date);
-  } else if (typeof VersionFiltred.data[process.platform] === "object") {
-    if (typeof VersionFiltred.data[process.platform][process.arch] === "string") {
-      DataReturn.url = VersionFiltred.data[process.platform][process.arch];
-      DataReturn.version = VersionFiltred.version;
-      DataReturn.Date = new Date(VersionFiltred.Date);
-    } else {
-      const ArchFiltred = Object.keys(VersionFiltred.data[process.platform]).filter(Arch => !Arch);
-      throw new Error(`Architecture ${ArchFiltred} not found`);
+/**
+ * Look for server versions with callback, returning an object with the latest server versions, in addition to updating the file locally
+ */
+function findVersionCallback(ServerVersion = "latest", ServerPlatform = "bedrock", Callback = (err = null, Data = {url: String(), GetBuffer: () => Buffer.from("a"), version: String(), Date: Date()}) => console.log(err, Data)) {
+  new Promise(async resolve => {
+    let VersionsList;
+    try {
+      VersionsList = (await Axios.get(`${GithubRawUrl}/src/Versions.json`)).data;
+    } catch (err) {
+      const Erro = new Error("Error getting the list of versions");
+      Erro.Error_raw = err;
+      Callback(Erro);
+      return;
     }
-  } else {
-    const PlatformFiltred = Object.keys(VersionFiltred.data).filter(Platform => !Platform);
-    throw new Error(`Platform ${PlatformFiltred} not found`);
-  }
-  return DataReturn;
+    try {
+      fs.writeFileSync(path.resolve(__dirname, "../Versions.json"), JSON.stringify(VersionsList, null, 2));
+      Callback(null, MainFunctionFind(ServerVersion, ServerPlatform, VersionsList));
+    } catch (err) {
+      Callback(err);
+    }
+    resolve();
+  });
 }
+Mod.findCallback = findVersionCallback;
 
-Mod.findCallback = function(ServerVersion = "latest", ServerPlatform = "bedrock", Callback = (err = null, Data = {url: String(), GetBuffer: () => Buffer.from("a"), version: String(), Date: Date()}) => console.log(err, Data)) {
-  Mod.findAsync(ServerVersion, ServerPlatform).then(Data => Callback(null, Data)).catch(err => Callback(err, {}));
-}
+/**
+ * Look for server versions with async, returning an object with the latest server versions, in addition to updating the file locally
+ */
+Mod.findAsync = util.promisify(findVersionCallback);
 
 // Export Modules
 module.exports = Mod;
