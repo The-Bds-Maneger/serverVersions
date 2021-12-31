5fb2d23dbc3b1c247dbd2885d1865cc9da262145
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2021-12-31 20:38:16 -0300

    Update index.js

--- src/cjs/index.js
+++ src/cjs/index.js
@@ -14,53 +14,79 @@ const GithubRawUrl = `https://raw.githubusercontent.com/The-Bds-Maneger/ServerVe
 const Mod = {};
 
 function MainFunctionFind(ServerVersion = "latest", ServerPlatform = "bedrock", VersionsList = require("../Versions.json")) {
-  if (typeof ServerVersion !== "string") throw new Error("ServerVersion must be a string");
-  if (typeof ServerPlatform !== "string") throw new Error("ServerPlatform must be a string");
-  ServerVersion = ServerVersion.toLowerCase(); ServerPlatform = ServerPlatform.toLowerCase();
-  if (ServerVersion === "latest") {
-    ServerVersion = VersionsList.latest[ServerPlatform];
-  }
-  //
-  // ------------------------------------------------------------------------------------------------------------
-  // Script
-  //
-  const VersionFiltred = (VersionsList.platform.filter(SV => SV.version === ServerVersion && SV.name === ServerPlatform))[0];
-  if (VersionFiltred === undefined) throw new Error("Version not found");
   const DataReturn = {
-    ".raw": VersionFiltred,
     url: "",
     version: "",
-    Date: new Date(),
+    Date: new Date()
   }
-  if (typeof VersionFiltred.data.url === "string") {
-    DataReturn.url = VersionFiltred.data.url;
-    DataReturn.version = VersionFiltred.version;
-    DataReturn.Date = new Date(VersionFiltred.Date);
-  } else if (typeof VersionFiltred.data[process.platform] === "object") {
-    if (typeof VersionFiltred.data[process.platform][process.arch] === "string") {
-      DataReturn.url = VersionFiltred.data[process.platform][process.arch];
+  if (typeof ServerPlatform !== "string") throw new Error("ServerPlatform must be a string");
+
+  if (typeof ServerVersion === "string") {
+    ServerVersion = ServerVersion.toLowerCase(); ServerPlatform = ServerPlatform.toLowerCase();
+    if (ServerVersion === "latest") {
+      ServerVersion = VersionsList.latest[ServerPlatform];
+    }
+    //
+    // ------------------------------------------------------------------------------------------------------------
+    // Script
+    //
+    const PlatformFilted = VersionsList.platform.filter(SV => SV.name === ServerPlatform);
+    if (PlatformFilted.length === 0) throw new Error(`Platform ${ServerPlatform} not found`);
+    const VersionFiltred = PlatformFilted.filter(SV => SV.version === ServerVersion)[0];
+    if (VersionFiltred === undefined) throw new Error("Version not found");
+    DataReturn.raw_request = VersionFiltred;
+    if (typeof VersionFiltred.data === "string") {
+      DataReturn.url = VersionFiltred.data;
       DataReturn.version = VersionFiltred.version;
       DataReturn.Date = new Date(VersionFiltred.Date);
+    } else if (typeof VersionFiltred.data[process.platform] === "object") {
+      if (typeof VersionFiltred.data[process.platform][process.arch] === "string") {
+        DataReturn.url = VersionFiltred.data[process.platform][process.arch];
+        DataReturn.version = VersionFiltred.version;
+        DataReturn.Date = new Date(VersionFiltred.Date);
+      } else {
+        const ArchFiltred = Object.keys(VersionFiltred.data[process.platform]).filter(Arch => !Arch);
+        throw new Error(`Architecture ${ArchFiltred} not found`);
+      }
     } else {
-      const ArchFiltred = Object.keys(VersionFiltred.data[process.platform]).filter(Arch => !Arch);
-      throw new Error(`Architecture ${ArchFiltred} not found`);
+      const Err = new Error("Error getting the version");
+      Err.Error_raw = VersionFiltred;
+      throw Err;
     }
-  } else {
-    const PlatformFiltred = Object.keys(VersionFiltred.data).filter(Platform => Platform);
-    throw new Error(`Platform ${PlatformFiltred.join(", ")} not found`);
-  }
+  } else throw new Error("ServerVersion must be a string");
   return DataReturn;
 }
 
 /**
  * Look for a server version with cached files saved from when the package was installed and when the findCallback and findAsync function is used
+ * 
+ * examples:
+ * 
+ *  find("1.16.1", "java");
+ * 
+ *  find("latest", "bedrock");
+ * 
+ *  find("latest", "java");
+ * 
  */
 Mod.find = (ServerVersion = "latest", ServerPlatform = "bedrock") => MainFunctionFind(ServerVersion, ServerPlatform, JSON.parse(fs.readFileSync(path.resolve(__dirname, "../Versions.json"), "utf8")));
 
 /**
  * Look for server versions with callback, returning an object with the latest server versions, in addition to updating the file locally
+ * 
+ * examples:
+ * 
+ * findCallback("1.16.1", "java", (err, data) => {
+ *   if (err) return console.log(err);
+ *   console.log(data);
+ * });
+ * 
+ * findCallback("latest", "bedrock", (err, data) => {
+ *  if (err) return console.log(err);
+ *  console.log(data);
+ * });
  */
-function findVersionCallback(ServerVersion = "latest", ServerPlatform = "bedrock", Callback = (err = null, Data = {url: String(), GetBuffer: () => Buffer.from("a"), version: String(), Date: Date()}) => console.log(err, Data)) {
+function findCallback(ServerVersion = "latest", ServerPlatform = "bedrock", Callback = (err = null, Data = {url: String(), GetBuffer: () => Buffer.from("a"), version: String(), Date: Date()}) => console.log(err, Data)) {
   new Promise(async resolve => {
     let VersionsList;
     try {
@@ -80,12 +106,20 @@ function findVersionCallback(ServerVersion = "latest", ServerPlatform = "bedrock
     resolve();
   });
 }
-Mod.findCallback = findVersionCallback;
+Mod.findCallback = findCallback;
 
 /**
  * Look for server versions with async, returning an object with the latest server versions, in addition to updating the file locally
+ * 
+ * examples:
+ * 
+ *  const Version = await findAsync("1.16.1", "java");
+ * 
+ *  const Version = await findAsync("latest", "bedrock");
+ * 
+ *  const Version = await findAsync("latest", "java");
  */
-Mod.findAsync = util.promisify(findVersionCallback);
+Mod.findAsync = util.promisify(findCallback);
 
 // Export Modules
 module.exports = Mod;
