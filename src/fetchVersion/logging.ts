d11bc3c0a2f85f397510f59fc86a0d80d194de2b
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-05-18 19:14:32 -0300

    Update find log colors

--- src/fetchVersion/logging.ts
+++ src/fetchVersion/logging.ts
@@ -1,6 +1,10 @@
-import { blue, green, red, redBright, yellow, yellowBright, cyan, cyanBright } from "cli-color";
+import { blue, green, red, yellow, yellowBright, cyan, cyanBright } from "cli-color";
 
 export default function Logging(colorTo: "java"|"bedrock"|"alter", message: string, ...args: any[]) {
+  if (message.toLowerCase().includes("already exists")||args.filter(x => typeof x === "string").find(x => x.includes("already exists"))) {
+    console.log(red(message), ...args);
+    return;
+  }
   if (colorTo === "java") {
     console.log(blue(message), ...args);
     return;
@@ -9,7 +13,7 @@ export default function Logging(colorTo: "java"|"bedrock"|"alter", message: stri
     return;
   } else {
     // Sort color random
-    const colors = [red, redBright, yellow, yellowBright, cyan, cyanBright];
+    const colors = [yellow, yellowBright, cyan, cyanBright];
     const color = colors[Math.floor(Math.random() * colors.length)];
     console.log(color(message), ...args);
     return;
