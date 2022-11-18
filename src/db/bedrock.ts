09074aceceb3fce65eea8cf168d24d1dfbc414fb
Matheus Sampaio Queiroga <srherobrine20@gmail.com>
2022-11-17 21:38:31 -0300

    Update bedrock.ts

--- src/db/bedrock.ts
+++ src/db/bedrock.ts
@@ -5,6 +5,7 @@ export const app = Router();
 export type bedrockSchema = {
   version: string,
   date: Date,
+  release?: string,
   url: {
     [platform in NodeJS.Platform]?: {
       [arch in NodeJS.Architecture]?: string
@@ -17,11 +18,11 @@ export async function getAll() {
 }
 
 app.get("/", ({res}) => getAll().then(data => res.json(data)));
-app.get("/latest", async ({res}) => res.json((await getAll()).at(-1)));
+app.get("/latest", async (req, res) => res.json((await getAll()).filter(rel => (req.query.beta?(rel.release === "preview"):true)).at(-1)));
 app.get("/search", async (req, res) => {
   let version = req.query.version as string;
   if (!version) return res.status(400).json({error: "No version specified"});
-  const versionFinded = (await getAll()).find(rel => rel.version === version);
+  const versionFinded = (await getAll()).find(rel => (rel.version === version) && (req.query.beta?(rel.release === "preview"):true));
   if (!versionFinded) return res.status(404).json({error: "Version not found"});
   return res.json(versionFinded);
 });
